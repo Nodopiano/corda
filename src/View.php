76d8bcec6374dca11f4ed9fd71c6235c7191fb38
Marco Bonomo <marco.bonomo@gmail.com>
2016-12-02 11:39:43 +0100

    global twig variables for analytics, facebook, hotjat

--- src/View.php
+++ src/View.php
@@ -24,6 +24,7 @@ class View implements Response
   {
         Twig_Autoloader::register();
         $this->twig = new Twig_Environment(new Twig_Loader_Filesystem('../app/views'));
+        $this->addTwigGlobals();
         $this->view = $view;
         $this->data = $data;
   }
@@ -36,6 +37,15 @@ class View implements Response
   public function render()
   {
     echo $this->twig->loadTemplate($this->view)->render($this->data);
+  }
+
+  public function addTwigGlobals()
+  {
+        //Add avaialble language to twig template as a global variable
+        $this->twig->addGlobal('analytics', getenv('ANALYTICS'));
+        $this->twig->addGlobal('facebook', getenv('FACEBOOK'));
+        $this->twig->addGlobal('hotjar', getenv('HOTJAR'));
+        $this->twig->addGlobal('mybusiness', getenv('MYBUSINESS'));
   }
 
 }
